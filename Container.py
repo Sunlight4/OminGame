b8a9f5238d3b0c35102058374053fc6873773d75
iahuang <ianhuang@bag-end.home>
2015-07-31 20:29:37 -0400

    Stuffs

--- Container.py
+++ Container.py
@@ -1,5 +1,9 @@
-import Item
+import Item,pygame
 
 class Container:
     def __init__(self,items):
         self.items = items
+class Chest:
+    def __init__(self,image,x,y,items):
+        self.image = pygame.image.load(image)
+        
